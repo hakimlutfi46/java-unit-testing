809d7ae763ac524cf9c81ee327d130bbf84e1541
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-11-01 22:15:05 +0700

    add comment in code

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,13 +4,16 @@ import java.util.stream.DoubleStream;
 
 public class Calculator {
 
+//    Method untuk penjumlahan
     static double add(double... operands) {
-        return DoubleStream.of(operands)
-                .sum();
+        // Menggunakan DoubleStream untuk menjumlahkan semua operan
+        return DoubleStream.of(operands).sum();
     }
 
+//    Meethod untuk perkalian
     static double multiply(double... operands) {
-        return DoubleStream.of(operands)
-                .reduce(1, (a, b) -> a * b);
+        // Menggunakan DoubleStream dan reduce() untuk mengalikan semua operan
+        // Nilai awal adalah 1, sehingga operasi perkalian dimulai dari 1
+        return DoubleStream.of(operands).reduce(1, (a, b) -> a * b);
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,21 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+//  Pengujian operasi penjumlahan
     @Test
     @DisplayName("Add two numbers")
     void add() {
 
+        // Memastikan bahwa hasil penjumlahan dari 2 + 2 adalah 4
         assertEquals(4, Calculator.add(2, 2));
     }
 
+//  Pengujian operasi perkalian
     @Test
     @DisplayName("Multiply two numbers")
     void multiply() {
 
         assertAll(
+
+                // Memastikan bahwa hasil perkalian dari 2 * 2 adalah 4
                 () -> assertEquals(4, Calculator.multiply(2, 2)),
+
+                // Pengujian untuk perkalian dengan angka negatif
                 () -> assertEquals(-4, Calculator.multiply(2, -2)),
+
+                // Pengujian untuk perkalian dua angka negatif
                 () -> assertEquals(4, Calculator.multiply(-2, -2)),
+
+                // Pengujian untuk perkalian dengan angka 0
                 () -> assertEquals(0, Calculator.multiply(1, 0)));
     }
 }
